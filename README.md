ece195e62d0092f98a1eaf1e1a45358fe9462094
Kit <ckyeungac@gmail.com>
2021-11-23 20:31:02 +0800

    refactor folder structure

--- README.md
+++ README.md
@@ -13,23 +13,23 @@ This repo aim to ease the setup effort of a new mac by running
 
 ## Setting up Github SSH Key
 
-1. Copy `scripts/setup_git_default.sh` to `scripts/setup_git.sh`.
+1. Copy `setup_git_default.sh` to `setup_git.sh`.
 2. Change the `NAME`, `EMAIL` and `PASSPHRASE` in `scripts/setup_git.sh`.
 3. Run
 
     ~~~sh
-    zsh scripts/setup_git.sh
+    zsh setup_git.sh
     ~~~
 
-There is manual input from you to autheticate on Github when running `scripts/setup_git.sh`.
+There is manual input from you to autheticate on Github when running `setup_git.sh`.
 
 Reference: https://docs.github.com/en/authentication/connecting-to-github-with-ssh
 
 ## Setting up VSCode Plugin
 
-1. Comment out any unwanted app in `scripts/install_vscode_plugin.sh`
+1. Comment out any unwanted app in `install_vscode_plugin.sh`
 2. Run
 
     ~~~sh
-    zsh scripts/install_vscode_plugin.sh
+    zsh install_vscode_plugin.sh
     ~~~
---
+++ install_vscode_plugin.sh
@@ -0,0 +1,71 @@
+#!/bin/zsh
+
+# check if visual studio code is installed.
+if [ ! -d "/Applications/Visual Studio Code.app"]; then
+    echo "Visual Studio Code is not installed. Exit now."
+    exit 1
+fi
+
+# enable `code` command is installed in Path if it is not done yet.
+if [ ! -f "/usr/local/bin/code" ]; then
+    sudo ln -fs "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /usr/local/bin/
+fi
+
+#   -----------------------------
+#   Productivity
+#   ----------------------------- 
+code --install-extension file-icons                             # prettify file icon 
+code --install-extension formulahendry.code-runner              # run a code snippet
+code --install-extension shan.code-settings-sync                # sync vscode setting cross device
+code --install-extension eamodio.gitlens                        # navigate code change in git
+code --install-extension donjayamanne.githistory                # trace git commits, message, branches, etc.
+code --install-extension ms-vscode-remote.remote-ssh            # open folder on remote machine with SSH
+code --install-extension ms-vscode-remote.remote-containers     # open folder inside a docker container
+code --install-extension ritwickdey.LiveServer                  # launch local development servers
+
+#   -----------------------------
+#   Formatting and Linting
+#   ----------------------------- 
+code --install-extension oderwat.indent-rainbow             # general
+code --install-extension esbenp.prettier-vscode             # general linting
+code --install-extension dbaeumer.vscode-eslint             # JS
+code --install-extension kevinrose.vsc-python-indent        # python
+code --install-extension davidanson.vscode-markdownlint     # mardown
+code --install-extension hex-ci.stylelint-plus              # CSS
+code --install-extension redhat.vscode-yaml                 # yaml
+code --install-extension mikestead.dotenv                   # .env
+
+#   -----------------------------
+#   Debugging
+#   ----------------------------- 
+code -install-extension streetsidesoftware.code-spell-checker       # spell checking 
+code -install-extension chrmarti.regex                              # regex previewer
+code -install-extension hbenl.vscode-test-explorer                  # UI for test
+
+#   -----------------------------
+#   Tool-specific 
+#   -----------------------------
+
+# python
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
+code --install-extension njpwerner.autodocstring
+
+# web development
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension naumovs.color-highlight
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension DigitalBrainstem.javascript-ejs-support
+code --install-extension erikphansen.vscode-toggle-column-selection
+
+# markdown
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension mushan.vscode-paste-image
+
+# docker
+code --install-extension ms-azuretools.vscode-docker
+
+# terraform
+code --install-extension hashicorp.terraform
